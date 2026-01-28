4f0a893c99edce410b8283974783e5171efb3544
Anuj Kamal Jain <23ucs540@lnmiit.ac.in>
2026-01-28 22:55:16 +0530

    feat: Initialize React application entry point with Redux store and service worker unregistration.

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./utils/appStore.js";
 
+// Unregister any existing service workers (cleanup for users who visited before)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+    });
+  });
+}
+
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
